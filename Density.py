db24e4f3ec9a9e9888c0c29e43d7fbf002f32539
mdbrannock <daniel.brannock@elderresearch.com>
2017-11-05 13:08:05 -0500

    Added next attempt using iterative density approximations for distributions

---
+++ Density.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  5 12:17:31 2017
+
+@author: mdbrannock
+"""
+
+# Second Attempt. Using density approximations to build iterative posterior
+# distributions.
+import numpy as np
+from sklearn.neighbors import KernelDensity
+from sklearn.grid_search import GridSearchCV
+
+# Define dictionary with starting prior distribution for every player.
+# The original prior will just be a normal distribution [u, s]. However, to
+# keep the formatting consistent, it would be nice to have the type of object
+# be the same for the first prior as all the other priors. As a result, we'll
+# build a density approximation of a normal distribution and use that.
+
+# Build density approximation using scikit learn:
+# https://jakevdp.github.io/blog/2013/12/01/kernel-density-estimation/
+# Well, nevermind, that's deprecated. New version:
+# http://scikit-learn.org/stable/modules/cross_validation.html
+
+
+
+# Define players
+players = {'aric':   [15, 3],
+           'daniel': [15, 3],
+           'jenn':   [15, 3],
+           'keith':  [15, 3],
+           'tom':    [15, 3]}
