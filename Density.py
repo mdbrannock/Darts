641808b21c72da0460adbfda1c76e1d8f5380c2d
Daniel Brannock <daniel.brannock@elderresearch.com>
2017-11-10 13:52:34 -0500

    Added cross validation for bandwidth calculation

--- Density.py
+++ Density.py
@@ -9,7 +9,7 @@ Created on Sun Nov  5 12:17:31 2017
 # distributions.
 import numpy as np
 from sklearn.neighbors.kde import KernelDensity
-from sklearn.grid_search import GridSearchCV
+from sklearn.model_selection import cross_val_score
 
 # Define dictionary with starting prior distribution for every player.
 # The original prior will just be a normal distribution [u, s]. However, to
@@ -18,19 +18,26 @@ from sklearn.grid_search import GridSearchCV
 # build a density approximation of a normal distribution and use that.
 
 # Build density approximation using scikit learn:
-# https://jakevdp.github.io/blog/2013/12/01/kernel-density-estimation/
-# Well, nevermind, that's deprecated. New version:
 # http://scikit-learn.org/stable/modules/cross_validation.html
 
 # Use CV to get best bandwidth. Use kde.score() as the evualation metric.
-X = np.array(np.random.normal(15, 3, 100))[:, np.newaxis]
-kde = KernelDensity(bandwidth=0.2).fit(X)
+X = np.array(np.random.normal(15, 3, 1000)).reshape(-1, 1)
+upper = 1.06*X.std()
+lower = 1.06*X.std()/20
+rng = np.arange(lower, upper, (upper-lower)/20)
+bws = {}
 
+for bw in rng:
+    kde = KernelDensity(bandwidth=bw)
+    s = cross_val_score(kde, X, cv=5).mean()
+    bws[bw] = s
+    
+fbw = max(bws.keys(), key = lambda x: bws[x])
+kde = KernelDensity(bandwidth = fbw).fit(X)
 
-
-# Define players
-players = {'aric':   [15, 3],
-           'daniel': [15, 3],
-           'jenn':   [15, 3],
-           'keith':  [15, 3],
-           'tom':    [15, 3]}
+# Define players' prior distributions
+players = {'aric':   kde,
+           'daniel': kde,
+           'jenn':   kde,
+           'keith':  kde,
+           'tom':    kde}
